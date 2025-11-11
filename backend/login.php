9aa31456b4b34fa245c9853d623b37a183911ba5
EzraKL <kipchumbaezra9@gmail.com>
2025-11-11 09:59:22 +0300

    Remove comment about file structure

--- backend/login.php
+++ backend/login.php
@@ -1,5 +1,5 @@
 <?php
-// Must be at the top of both register.php and login.php
+
 header('Access-Control-Allow-Origin: *'); 
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE'); 
 header('Access-Control-Allow-Headers: Content-Type');
